ba317cc1fe7ca5bc72b134ec1ab12c3e22f14196
Jimmy Jurado <jurado140@gmail.com>
2023-08-06 14:56:24 -0400

    adds recipes controller.

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::RecipesController < ApplicationController
   def index
     fetch_recipe = RecipeSearchFacade.new(params).recipes
+    fetch_recipe = [fetch_recipe] unless fetch_recipe.is_a?(Array)
     render json: ApiRecipeSerializer.new(fetch_recipe)
   end
 end

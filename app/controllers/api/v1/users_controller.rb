7e37f8e34164fe4ea47a48f62b3ac5d62bf6ad93
Jimmy Jurado <jurado140@gmail.com>
2023-08-08 11:38:39 -0400

    adds user controller with happy and sad paths.

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: UserSerializer.new(user), status: 201
+    else
+      ErrorUserSerializer.new("Bad Credentials", 404)
+    end
+  end
+
+  private
+  
+  def user_params
+    params.permit(:name, :email, :password, :password_confirmation)
+  end
+end
